0f2c06aa2d3a5c39ed77f2b467278e30e555474f
Jason A. Wise <jwise.developer@gmail.com>
2021-03-26 23:25:01 -0400

    working on styling the nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import { FaFacebook } from 'react-icons/fa'
 
 const Nav = () => (
-    <nav className="bg-white dark:bg-gray-700">
-      <ul>
-        <li><FaFacebook / ></li>
+    <nav className="bg-white dark:bg-gray-700 flex py-2 px-4 border-solid border-b">
+      <ul className="flex">
+        <li><FaFacebook className="text-4xl" fill="#0b88ef"/ ></li>
         <li>SEARCH</li>
       </ul>
-      <ul>
+      <ul className="flex">
         <li>Home</li>
         <li>Friends</li>
         <li>Groups</li>
       </ul>
-      <ul>
+      <ul className="flex">
         <li>Create</li>
         <li>Messenger</li>
         <li>Notifications</li>
