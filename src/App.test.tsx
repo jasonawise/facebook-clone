2dcbe1485a0e4815fdba551f36fcb3b87ca993c2
Jason A. Wise <jwise.developer@gmail.com>
2021-03-26 22:23:50 -0400

    first commit

---
+++ src/App.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+// eslint-disable-next-line import/no-unresolved
+import App from './App'
+
+test( `renders learn react link`, () => {
+  render( <App /> )
+  const linkElement = screen.getByText( /learn react/i )
+  expect( linkElement ).toBeInTheDocument()
+} )
